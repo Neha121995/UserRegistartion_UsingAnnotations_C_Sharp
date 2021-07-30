1f1bf47c0fe74524c4da2545b0f0b126ccfe1714
Neha121995 <jagtapneha1212@gmail.com>
2021-07-30 18:56:30 +0530

    [Neha] Added: User Registartion Using Annotations for Password (Rule1 : Minimum 8 character)

--- AnnotationsTest.cs
+++ AnnotationsTest.cs
@@ -13,7 +13,7 @@ namespace UserRegistration_UsingAnnotations
             Console.WriteLine("User Registration Mobile Number Validations");
             Console.WriteLine("===============================");
             User objUser = new User();
-            objUser.mobileNumber = "9";
+            objUser.password = "abcxyz";
             ValidationContext context = new ValidationContext(objUser, null, null);
             List<ValidationResult> results = new List<ValidationResult>();
             bool valid = Validator.TryValidateObject(objUser, context, results, true);
@@ -21,13 +21,13 @@ namespace UserRegistration_UsingAnnotations
             {
                 foreach (ValidationResult TotalResult in results)
                 {
-                    Console.WriteLine("Mobile Number is :: {0}", TotalResult.MemberNames.First(), Environment.NewLine);
+                    Console.WriteLine("Password is :: {0}", TotalResult.MemberNames.First(), Environment.NewLine);
                     Console.WriteLine("Error Msg :: {0}{1}", TotalResult.ErrorMessage, Environment.NewLine);
                 }
             }
             else
             {
-                Console.WriteLine("Mobile Number is :: " + objUser.mobileNumber);
+                Console.WriteLine("Password is :: " + objUser.password);
             }
             Console.WriteLine("\nPress any key to exit");
             Console.ReadKey();
--- User.cs
+++ User.cs
@@ -8,8 +8,8 @@ namespace UserRegistration_UsingAnnotations
     class User
     {
         [Required(ErrorMessage = "User {0} is required")]
-        [StringLength(12, MinimumLength = 10, ErrorMessage = "Please enter valid mobile number")]
-        [DataType(DataType.PhoneNumber)]
-        public string mobileNumber { get; set; }
+        [RegularExpression(@"^[A-Za-z]{8,}", ErrorMessage = "Please enter minimum 8 character")]
+        [DataType(DataType.Password)]
+        public string password { get; set; }
     }
 }
