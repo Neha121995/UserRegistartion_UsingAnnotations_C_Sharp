bf652926debfb6a1d386aa1bc05ffe3273be6c1d
Neha121995 <jagtapneha1212@gmail.com>
2021-07-30 17:52:33 +0530

    [Neha] Added: Welcome to User Registartion Practice Problem Using Annotation

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserRegistration_UsingAnnotations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registartion Practice Problem Using Annotation");
+        }
+    }
+}
